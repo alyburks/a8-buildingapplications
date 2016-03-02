dc8443f8b3c39ea48baf37c80cc124c0d74247e9
Alyxis Burks <alyxistarynburks@gmail.com>
2016-03-01 18:31:55 -0800

    heres the most recent

--- a8/server.R
+++ a8/server.R
@@ -1,6 +1,7 @@
 
 library(shiny)
 library(plotly)
+library(rsconnect)
 data("iris")
 
 shinyServer(function(input, output) {
--- a8/ui.R
+++ a8/ui.R
@@ -1,6 +1,7 @@
 
 library(shiny)
 library(plotly)
+library(rsconnect)
 
 
 shinyUI(fluidPage(
